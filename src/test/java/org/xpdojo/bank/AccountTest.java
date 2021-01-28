b636e81ebb27fe6571f49db717fe3bc339283a14
Ionel Nita <ionel.nita@gcp.deuba.com>
2021-01-28 17:05:13 +0200

    first tests

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountsHaveABalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void depositAnAmount() {
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance()).isEqualTo(100);
+    }
+
+    @Test
+    public void depositMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(200);
+        assertThat(account.balance()).isEqualTo(300);
     }
 }
